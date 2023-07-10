fe03ea535deb0edd5a14bb798f84113f21d44fba
Nishant Karlupia <nishantkarlupia149@gmail.com>
2023-07-10 14:15:56 +0530

    dashboard updated

--- split_data.py
+++ split_data.py
@@ -1,8 +1,50 @@
 import sys
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QMainWindow,QWidget,QApplication,QVBoxLayout,QLineEdit,QHBoxLayout,QMessageBox,QLabel,QGridLayout
-from CustomWidgets import CustomListWidget,FirstButton
+import typing
+from PyQt5.QtCore import QObject, Qt,QThread,pyqtSignal
+from PyQt5.QtWidgets import QMainWindow,QWidget,QApplication,QVBoxLayout,QLineEdit,QHBoxLayout,QMessageBox,QLabel,QGridLayout,QFrame,QTextEdit
+from CustomWidgets import CustomListWidget,FirstButton,CustomMessageBox
 from CustomFunction import apply_stylesheet,Open_Datafile
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error,mean_absolute_error,r2_score
+from globalParams.stateStore import store
+from globalParams.dataStore import globalData
+
+
+class Worker(QThread):
+
+    matrix=pyqtSignal(dict)
+
+    def __init__(self,x_train,y_train,x_test,y_test):
+        super().__init__()
+        self.x_train=x_train
+        self.y_train=y_train
+        self.x_test=x_test
+        self.y_test=y_test
+
+    def run(self):
+        try:
+            reg=LinearRegression()
+            reg.fit(self.x_train,self.y_train)
+            y_pred=reg.predict(self.x_test)
+            mse=mean_squared_error(self.y_test,y_pred)
+            mae=mean_absolute_error(self.y_test,y_pred)
+            cod=r2_score(self.y_test,y_pred)
+            score=reg.score(self.x_test,self.y_test)
+
+            matrix={
+                "Score":round(score,3),
+                "Coefficient of determination":round(cod,3),
+                "Mean Absolute Error":round(mae,3),
+                "Mean Squared Error":round(mse,3)
+            }
+
+            self.matrix.emit(matrix)
+        except:
+            self.matrix.emit({})
+
+
+
 
 class SplitWindow(QMainWindow):
     def __init__(self,columns,x_col,y_col):
@@ -11,6 +53,7 @@ class SplitWindow(QMainWindow):
         self.setWindowFlags(Qt.FramelessWindowHint)
         self.setMinimumSize(750,380)
 
+        
         self.list1=CustomListWidget()
         self.list1.addItems(columns)
         self.list1.currentItemChanged.connect(self.data_change_occur)
@@ -21,6 +64,12 @@ class SplitWindow(QMainWindow):
         self.list2.currentItemChanged.connect(self.data_change_occur)
         target_label=QLabel("Target Variables")
         target_label.setObjectName("target")
+
+        self.notRequired=CustomListWidget()
+        self.notRequired.currentItemChanged.connect(self.data_change_occur)
+        nrdatalabel=QLabel("Not Required")
+        nrdatalabel.setObjectName("nrdatalabel")
+
 
         
 
@@ -37,6 +86,8 @@ class SplitWindow(QMainWindow):
         item_layout.addWidget(self.list1,1,0)
         item_layout.addWidget(target_label,0,1)
         item_layout.addWidget(self.list2,1,1)
+        item_layout.addWidget(nrdatalabel,0,2)
+        item_layout.addWidget(self.notRequired,1,2)
 
         btn_layout=QHBoxLayout()
         btn_layout.addWidget(self.confirm_btn)
@@ -44,6 +95,7 @@ class SplitWindow(QMainWindow):
 
         layout.addLayout(item_layout)
         layout.addLayout(btn_layout)
+
         widget.setLayout(layout)
         self.setCentralWidget(widget)
 
@@ -70,8 +122,10 @@ class MainWindow(QMainWindow):
         super().__init__()
 
         self.df,self.data_split,self.x_col,self.y_col=None,None,[],[]
+        self.msg_box=None
+        self.df=globalData.give_data()
 
-        self.setWindowTitle("Show the data")
+        self.setWindowTitle("Model Trainig")
         self.setMinimumSize(500,500)
 
         widget=QWidget()
@@ -79,8 +133,31 @@ class MainWindow(QMainWindow):
         
         
         self.sep=QLineEdit()
+
         self.sep.setPlaceholderText("Enter the separator (; , ...etc)")
         self.sep.setObjectName("separator")
+
+
+        frame=QFrame()
+        self.train_btn=FirstButton("Train","train_btn",self.train_model_function)
+        self.split_btn=FirstButton("Select Train Test","select_train_test_btn",self.open_data_split_window)
+        # self.train_btn.setDisabled(True)
+        self.train_report=QTextEdit()
+        self.train_report.setReadOnly(True)
+        if self.df is None:
+            self.train_report.setText("No data available")
+            self.split_btn.setDisabled(True)
+        else:
+            self.train_report.setText("select train and target variables")
+        self.train_report.setObjectName("train_report")
+        frame_layout=QVBoxLayout()
+        frame_layout.addWidget(self.train_report)
+        btn_layout=QHBoxLayout()
+        btn_layout.addWidget(self.split_btn)
+        btn_layout.addWidget(self.train_btn)
+        frame_layout.addLayout(btn_layout)
+        # frame_layout.setAlignment(self.train_btn, Qt.AlignHCenter)
+        frame.setLayout(frame_layout)
 
         hbox=QHBoxLayout()
         hbox.addWidget(self.open_btn)
@@ -88,18 +165,19 @@ class MainWindow(QMainWindow):
         
         layout=QVBoxLayout()
         layout.addLayout(hbox)
+        layout.addWidget(frame)
         widget.setLayout(layout)
 
 
         self.setCentralWidget(widget)
 
-        apply_stylesheet(self,'styles/split_data.qss')
+        apply_stylesheet(self,'styles/train_models.qss')
 
 
     def open_file(self):
-        if(len(self.x_col)!=0 and len(self.y_col)!=0):
-            print(self.x_col)
-            print(self.y_col)
+        # if(len(self.x_col)!=0 and len(self.y_col)!=0):
+        #     print(self.x_col)
+        #     print(self.y_col)
 
         res=Open_Datafile(self,self.sep)
 
@@ -107,22 +185,64 @@ class MainWindow(QMainWindow):
             return
         
         if res[0]==False:# file not an excel or csv file
-            QMessageBox.critical(self,"Error!!!","Make sure that file is .xlsx or .csv")
+            # QMessageBox.critical(self,"Error!!!","Make sure that file is .xlsx or .csv")
+            self.msg_box=CustomMessageBox("Error","Make sure that file is .xlsx or .csv")
+            store.add(self.msg_box)
+            self.msg_box.show()
             return
                 
         self.df=res[1]
+        globalData.assign_data(self.df)
             
         self.x_col,self.y_col=[],[]
+
+        # self.open_data_split_window()
+        self.split_btn.setDisabled(False)
+        self.train_report.setText("Select train and target variables")
+
+    def open_data_split_window(self):
         
         self.data_split=SplitWindow(list(self.df.columns),self.x_col,self.y_col)
+        store.add(self.data_split)
         self.data_split.show()
+
+    
+    def train_model_function(self):
+        # print("hello world")
+        if len(self.x_col)==0 or len(self.y_col)==0:
+            self.insufficient_data_error_msg=CustomMessageBox("InSufficient Data","Either train or target variable missing")
+            store.add(self.insufficient_data_error_msg)
+            self.insufficient_data_error_msg.show()
+            return
+        
+        X,y=self.df[list(self.x_col)],self.df[list(self.y_col)]
+        # print(X)
+        # print(y)
+        x_train,x_test,y_train,y_test=train_test_split(X,y,test_size=0.3)
+        
+        self.worker=Worker(x_train,y_train,x_test,y_test)
+        self.worker.matrix.connect(self.print_report)
+        self.worker.start()
+
+        pass
+
+    def print_report(self,matrix):
+        if len(matrix)==0:
+            self.trainig_error=CustomMessageBox("Error","Not able to train, check the data fields again!!!")
+            store.add(self.trainig_error)
+            self.trainig_error.show()
+            return
+        report=""
+        for key,val in matrix.items():
+            report+=str(key)+" : "+str(val)+"\n"
+            # print(key,val)
+        # print(matrix)
+        self.train_report.setText(report)
 
 
     # close all second-window opened
     def closeEvent(self,event):
-        if self.data_split!=None:
-            self.data_split.close()
-            
+        store.close()
     
 
 if __name__=="__main__":
